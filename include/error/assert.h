3a3025a26a6fa3f458170288161e998fcf192611
Leorevoir <leo.quinzler@epitech.eu>
2025-08-17 00:03:52 +0200

    feat: __assert now supports printf fmt

--- include/error/assert.h
+++ include/error/assert.h
@@ -12,23 +12,30 @@ static const char *CPLUS_LOG_YELLOW = "\033[0;33m";
 static const char *CPLUS_LOG_MAGENTA = "\033[0;35m";
 static const char *CPLUS_LOG_RESET = "\033[0m";
 
-static __inline void __cplus__assert(const char *file, int line, const char *func, const char *msg)
+static __inline void __cplus__assert(const char *file, int line, const char *func, const char *fmt, ...)
 {
+    va_list args;
+
     fprintf(stderr, "%s", CPLUS_LOG_RED);
     fprintf(stderr, "ERROR:%s\n", CPLUS_LOG_RESET);
     fprintf(stderr, "\tASSERTION FAILED");
     fprintf(stderr, " in %s:%d\n", file, line);
     fprintf(stderr, "%sFUNCTION:%s\n\t(%s)\n", CPLUS_LOG_MAGENTA, CPLUS_LOG_RESET, func);
-    fprintf(stderr, "%sTRACE_LOG:%s\n\t%s\n", CPLUS_LOG_YELLOW, CPLUS_LOG_RESET, msg);
+
+    fprintf(stderr, "%sTRACE_LOG:%s\n\t", CPLUS_LOG_YELLOW, CPLUS_LOG_RESET);
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    fprintf(stderr, "\n");
 
     collect_garbage;
     exit(84);
 }
 
-#define __assert(condition, msg)                                                                                       \
+#define __assert(condition, ...)                                                                                       \
     do {                                                                                                               \
         if (!(condition)) {                                                                                            \
-            __cplus__assert(__FILE__, __LINE__, __func__, msg);                                                        \
+            __cplus__assert(__FILE__, __LINE__, __func__, __VA_ARGS__);                                                \
         }                                                                                                              \
     } while (0)
 
