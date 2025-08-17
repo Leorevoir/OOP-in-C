cdf397015d91e663b7ef0feceb8a1a5c1e0fb58a
Leorevoir <leo.quinzler@epitech.eu>
2025-08-17 18:04:34 +0200

    fix: make hash_str an inline function to optimize it with attribute const

--- include/oop/string.h
+++ include/oop/string.h
@@ -1,6 +1,7 @@
 #ifndef CPLUS_OOP_H_
 #define CPLUS_OOP_H_
 
+#include <oop/macros.h>
 #include <stddef.h>
 
 typedef struct {
@@ -11,11 +12,14 @@ typedef struct {
 void sv_show(const StrView sv);
 void sv_copy(char *out_buff, size_t buf_size, const StrView sv);
 
-#define has_str(s)                                                                                                     \
-    size_t h = 5381;                                                                                                   \
-    for (const unsigned char *p = (const unsigned char *) s; *p; ++p) {                                                \
-        h = ((h << 5) + h) + *p;                                                                                       \
-    }                                                                                                                  \
-    [h]
+__cplus__const static __inline size_t hash_str(const char *s)
+{
+    size_t h = 5381;
+
+    for (const unsigned char *p = (const unsigned char *) s; *p; ++p) {
+        h = ((h << 5) + h) + *p;
+    }
+    return h;
+}
 
 #endif /* CPLUS_OOP_H_ */
