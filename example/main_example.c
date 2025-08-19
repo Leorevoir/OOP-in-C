33912bc49d76007229b73d665299ca75038f6e27
Leorevoir <leo.quinzler@epitech.eu>
2025-08-19 14:37:41 +0200

    update: example/main_example.c

--- example/main_example.c
+++ example/main_example.c
@@ -1,7 +1,38 @@
 #include <memory/allocate.h>
 #include <oop/array.h>
+#include <oop/map.h>
 
-int main(void)
+#include <stdio.h>
+
+char *str_tmp_buff(const char *str, const size_t index)
+{
+    char *buff;
+
+    allocate(buff, sizeof(char) * 20);
+    sprintf(buff, "%s%zu", str, index);
+    return buff;
+}
+
+void example_map(void)
+{
+    /** preallocate memory for 10000 elements to avoid reallocation */
+    Map *map = new (MapClass, 10000);
+
+    for (size_t i = 0; i < 10000; ++i) {
+        char *key = str_tmp_buff("key", i);
+        char *value = str_tmp_buff("value", i);
+
+        map->insert(map, key, value);
+    }
+
+    __assert(map->contains(map, "key5000"), "Expected map to contain key5000");
+    __assert(map->size(map) == 10000, "Expected size to be 10000, got %zu", map->size(map));
+    printf("%s\n", (const char *) map->get(map, "key5000"));
+
+    delete (map);
+}
+
+void example_array(void)
 {
     /** create an array of any type (can also be specific-only like const char*, int, struct...) */
     /** preallocate memory for 3 elements to avoid reallocation */
@@ -16,6 +47,12 @@ int main(void)
     array_foreach(array, any, str, { printf("%s ", (const char *) str); });
 
     __assert(array->size(array) == 3, "Expected size to be 3, got %zu", array->size(array));
+}
+
+int main(void)
+{
+    example_array();
+    example_map();
 
     /** no memory leaks because every allocation is handled by our garbage collector */
     return 0;
